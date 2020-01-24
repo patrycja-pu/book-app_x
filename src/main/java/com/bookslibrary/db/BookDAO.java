5c5351a461b03ac4d950ffdd11c782c5cd18a0f4
Piotr Konicki <konicki.piotr@gmail.com>
2020-01-24 09:30:07 +0100

    clean

--- src/main/java/com/bookslibrary/db/BookDAO.java
+++
@@ -1,9 +0,0 @@
-package com.bookslibrary.db;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bookslibrary.model.Book;
-
-public interface BookDAO extends JpaRepository<Book, Integer> {
-
-}
